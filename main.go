e3a3f090f45f9ad4a2a0c7d7d5706f1639854890
PersesTitan <torampersestitan@gmail.com>
2022-05-10 08:02:13 +0900

    comiit

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+//import "fmt"
+//
+//func main() {
+//	fmt.Println("강한친구 대한육군")
+//	fmt.Println("강한친구 대한육군")
+//}
+
+func sum(a []int) int {
+	s := 0
+	for value := range a {
+		s += a[value]
+	}
+	return s
+}
 
 func main() {
-	fmt.Println("강한친구 대한육군")
-	fmt.Println("강한친구 대한육군")
+	nums := []int{1, 5, 16, 2}
+	print(sum(nums))
 }
